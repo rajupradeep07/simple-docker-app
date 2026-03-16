635bd8e07a086452ce9e2fcc5c6c8c8af614b721
rajupradeep07 <rajupradeep07@gmail.com>
2026-03-16 15:34:10 +0530

    app.py changed

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ redis_client = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-        redis_client.incr('visits')
-            visits = redis_client.get('visits').decode('utf-8')
-                return f"Hello! You visited this page {visits} times."
+    redis_client.incr('visits')
+    visits = redis_client.get('visits').decode('utf-8')
+    return f"Hello! You visited this page {visits} times."
 
-            if __name__ == "__main__":
-                    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
