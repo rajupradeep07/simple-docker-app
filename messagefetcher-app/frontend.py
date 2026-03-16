78b312c1c93aa21f3e3087a004b5239c01ea3f94
rajupradeep07 <rajupradeep07@gmail.com>
2026-03-16 16:59:47 +0530

    Update frontend.py

--- messagefetcher-app/frontend.py
+++ messagefetcher-app/frontend.py
@@ -4,14 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def display_value():
+def display():
     try:
-        # It calls the second container by its name 'backend-node'
-        response = requests.get("http://backend-node:6000/get-value")
-        secret_value = response.text
-        return f"<h1>Container 1 (Frontend)</h1><p>Value fetched from Container 2: <b>{secret_value}</b></p>"
+        # Calls the backend service
+        response = requests.get("http://backend-service:6000/data")
+        # This will show up in your browser
+        return f"<h1>Frontend (Container 1)</h1><p>Data from Backend: <b>{response.text}</b></p>"
     except Exception as e:
-        return f"Failed to reach the 2nd container: {e}"
+        return f"Error: {e}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
